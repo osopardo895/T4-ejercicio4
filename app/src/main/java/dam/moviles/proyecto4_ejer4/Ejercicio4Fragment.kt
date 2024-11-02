9a16a39ffd619a5653726d4193ddc9ee852c6012
osopardo895 <alexcesarfer1234@gmail.com>
2024-11-02 18:35:37 +0100

    cambio funcionamiento de clase moneda a Maps<>

--- app/src/main/java/dam/moviles/proyecto4_ejer4/Ejercicio4Fragment.kt
+++ app/src/main/java/dam/moviles/proyecto4_ejer4/Ejercicio4Fragment.kt
@@ -12,17 +12,21 @@ import android.widget.ArrayAdapter
 import com.google.android.material.snackbar.Snackbar
 import dam.moviles.proyecto4_ejer4.databinding.FragmentEjercicio4Binding
 
-//CLASE MONEDA
-data class Moneda(val nombre:String,val valor:Double,val imagenViewRes:Int)
-
 class Ejercicio4Fragment : Fragment() {
     private lateinit var binding : FragmentEjercicio4Binding
-    private val listaMonedas:List<Moneda> = listOf(
-        Moneda("Euro",1.0,R.drawable.unioneuropea),
-        Moneda("Dólar",1.08,R.drawable.estadosunidos),
-        Moneda("Libra",0.83,R.drawable.reinounido),
-        Moneda("Yen",165.86,R.drawable.china)
-        )
+    private val valorMonedas:Map<String,Double> = mapOf(
+        "Euro" to 1.0,
+        "Dólar" to 1.08,
+        "Libra" to 0.83,
+        "Yen" to 165.86
+    )
+    private val imagenMonedas:Map<String,Int> = mapOf(
+        "Euro" to R.drawable.unioneuropea,
+        "Dólar" to R.drawable.estadosunidos,
+        "Libra" to R.drawable.reinounido,
+        "Yen" to R.drawable.china
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,16 +44,17 @@ class Ejercicio4Fragment : Fragment() {
     }
     private fun configurarSpinners() {
         val listaNombresMonedas:MutableList<String> = mutableListOf()
-        val adapter = ArrayAdapter<String>(requireContext(),android.R.layout.simple_list_item_1,listaNombresMonedas)
-        for (i in listaMonedas){
-            listaNombresMonedas.add(i.nombre)
+        for (i in valorMonedas.keys.toList()){
+            listaNombresMonedas.add(i)
         }
+        val adapter = ArrayAdapter<String>(requireContext(),android.R.layout.simple_list_item_1,listaNombresMonedas)
+
         binding.spnCambio.adapter = adapter
         binding.spnCambio.setSelection(0)
         binding.spnCambio.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, id: Long) {
-                val selectedItem = listaMonedas[position]
-                binding.imageViewSpnCambio.setImageResource(selectedItem.imagenViewRes)
+                val selectedItem:Int = imagenMonedas.values.toList().get(position)
+                binding.imageViewSpnCambio.setImageResource(selectedItem)
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -61,8 +66,8 @@ class Ejercicio4Fragment : Fragment() {
         binding.spnRecibir.setSelection(1)
         binding.spnRecibir.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, id: Long) {
-                val selectedItem = listaMonedas[position]
-                binding.imageViewSpnRecibir.setImageResource(selectedItem.imagenViewRes)
+                val selectedItem:Int = imagenMonedas.values.toList().get(position)
+                binding.imageViewSpnRecibir.setImageResource(selectedItem)
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -77,14 +82,14 @@ class Ejercicio4Fragment : Fragment() {
             val dinero:Editable = binding.edtDinero.text
             val cambio:String = binding.spnCambio.selectedItem.toString()
             val recibir:String = binding.spnRecibir.selectedItem.toString()
-            var monedaCambio:Moneda? = listaMonedas.find { it.nombre == cambio }
-            var monedaRecibir:Moneda? = listaMonedas.find { it.nombre == recibir }
+            var monedaCambio:Double? = valorMonedas.get(cambio)
+            var monedaRecibir:Double? = valorMonedas.get(recibir)
 
             if (monedaCambio != null && monedaRecibir!= null && dinero.isNotEmpty()) {
-            val dineroConvertido = DecimalFormat("#.00").format((dinero.toString().toDouble()*monedaRecibir.valor/monedaCambio.valor))
+            val dineroConvertido = DecimalFormat("#.00").format((dinero.toString().toDouble()*monedaRecibir/monedaCambio))
             Snackbar.make(
                 requireView(),
-                "Conversión: "+dineroConvertido+" "+monedaRecibir.nombre,
+                "Conversión: "+dineroConvertido+" "+recibir,
                 Snackbar.LENGTH_LONG
             ).show()
             }
